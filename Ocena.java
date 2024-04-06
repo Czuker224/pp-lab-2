204543f70f97a97293ae9aa2d783bc3ef47eb5d2
Czuker224 <56686494+Czuker224@users.noreply.github.com>
2024-04-06 14:50:16 +0200

    dodanie clasy Ocena + zmiany w pliku SredniaOcen

---
+++ Ocena.java
@@ -0,0 +1,17 @@
+class Ocena {
+    private String przedmiot;
+    private double ocena;
+
+    public Ocena(String przedmiot, double ocena) {
+        this.przedmiot = przedmiot;
+        this.ocena = ocena;
+    }
+
+    public String getPrzedmiot() {
+        return przedmiot;
+    }
+
+    public double getOcena() {
+        return ocena;
+    }
+}
--- SredniaOcen.java
+++ SredniaOcen.java
@@ -5,6 +5,10 @@ public class SredniaOcen {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Utworzenie tablicy na oceny
+        Ocena[] oceny = new Ocena[3];
+
+        // Pobranie danych od uzytkownika
         System.out.println("Podaj ocene z matematyki: ");
         double matematyka = scanner.nextDouble();
 
@@ -14,8 +18,19 @@ public class SredniaOcen {
         System.out.println("Podaj ocene z chemii: ");
         double chemia = scanner.nextDouble();
 
+        // wpisanie danych do tabeli
+        oceny[0] = new Ocena("matematyka", matematyka);
+        oceny[1] = new Ocena("fizyka", fizyka);
+        oceny[2] = new Ocena("fizyka", fizyka);
+
         // Oblicz średnią arytmetyczną
-        double srednia = (matematyka + fizyka + chemia) / 3;
+        double suma = 0;
+        for (int i = 0; i < oceny.length; i++) {
+            double num = oceny[i].getOcena();
+            suma += num;
+        }
+        double srednia = suma / oceny.length;
+
 
         // Wyświetl wynik
         System.out.println("Twoja srednia ocen z przedmiotow to: " + srednia);
