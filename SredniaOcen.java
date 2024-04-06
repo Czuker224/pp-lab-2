95a88fcebbd6565bd2a48f475735fe6236d22352
Czuker224 <56686494+Czuker224@users.noreply.github.com>
2024-04-06 15:17:39 +0200

    Dodanie funkcji sprawdającej założenia poprawności oceny

--- SredniaOcen.java
+++ SredniaOcen.java
@@ -9,24 +9,39 @@ public class SredniaOcen {
         Ocena[] oceny = new Ocena[3];
 
         // Pobranie danych od uzytkownika
+        int i = 0;
+
         System.out.println("Podaj ocene z matematyki: ");
         double matematyka = scanner.nextDouble();
+        if(sprawdzOcene(matematyka)){
+            oceny[i] = new Ocena("matematyka", matematyka);
+            i++;
+        }else{
+            System.out.println("Podana ocena jest niepoprawna. Wprowadź ocene dodatnia mniejsza od 6.");
+        }
 
         System.out.println("Podaj ocene z fizyki: ");
         double fizyka = scanner.nextDouble();
+        if(sprawdzOcene(fizyka)){
+            oceny[i] = new Ocena("matematyka", fizyka);
+            i++;
+        }else{
+            System.out.println("Podana ocena jest niepoprawna. Wprowadź ocene dodatnia mniejsza od 6.");
+        }
 
         System.out.println("Podaj ocene z chemii: ");
         double chemia = scanner.nextDouble();
-
-        // wpisanie danych do tabeli
-        oceny[0] = new Ocena("matematyka", matematyka);
-        oceny[1] = new Ocena("fizyka", fizyka);
-        oceny[2] = new Ocena("fizyka", fizyka);
+        if(sprawdzOcene(chemia)){
+            oceny[i] = new Ocena("matematyka", chemia);
+            i++;
+        }else{
+            System.out.println("Podana ocena jest niepoprawna. Wprowadź ocene dodatnia mniejsza od 6.");
+        }
 
         // Oblicz średnią arytmetyczną + wyświetla wyniki
         System.out.println("Lista ocen:");
         double suma = 0;
-        for (int i = 0; i < oceny.length; i++) {
+        for (i = 0; i < oceny.length; i++) {
             //wyświetla ocene z przedmiotu
             System.out.println(oceny[i].getPrzedmiot() + "( "+ oceny[i].getOcena() +" )");
 
@@ -38,5 +53,14 @@ public class SredniaOcen {
         System.out.println("Twoja srednia ocen z przedmiotow to: " + srednia);
 
         scanner.close(); // Zamknięcie obiektu Scanner
+    }
+
+    private static boolean sprawdzOcene(double ocena){
+        // Sprawdzenie warunku czy ocena jest dodatnia i mniejsza od 6
+        if (ocena > 0 && ocena < 6) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
